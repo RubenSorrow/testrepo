854f9db6b0f39781f7c6af02d9a3984671f6672b
Ruben Soro <61415949+RubenSorrow@users.noreply.github.com>
2024-02-05 17:13:57 +0100

    Add files via upload
    Adding local file for test

---
+++ untitled0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 17 12:32:27 2023
+
+@author: ruben
+"""
+gemsPerPlayer = 2.438710
+perc1 = 0.01
+perc2 = 0.09
+perc3 = 0.15
+gemsEur = 0.05
+
+def launch(n):
+    gemsDistributed = gemsPerPlayer*n
+    
